e6af0a98fa39f4996b051ef293363315a6c7b126
sukemomo <ttnarita@gmail.com>
2017-10-15 06:37:48 +0900

    Crawler完成

--- python_crawler_2.py
+++ python_crawler_2.py
@@ -6,14 +6,22 @@ import lxml.html
 from pymongo import MongoClient
 
 def main():
-    # 複数のページをクロールするのでSessionを使う。
-    session = requests.Session()
-    response = session.get('http://gihyo.jp/dp')
+    # クローラーのメイン処理
+    client = MongoClient('localhost', 27017)
+    collection = client.scraping.ebooks
+    # データを一意に識別するキーを格納するkeyフィールドにユニークなインデックスを作成する。
+    collection.create_index('key',unique=True)
+    response = requests.get('http://gihyo.jp/dp')
     urls = scrape_list_page(response)
     for url in urls:
-        time.sleep(1)
-        response = session.get(url) # Sessionを使って詳細ページを取得する。
-        ebook = scrape_detail_page(response)
+        key = extract_key(url)
+        ebook = collection.find_one({'key':key})
+        # MongoDBからKeyに該当するデータを探す。
+        if not ebook:
+            time.sleep(1)
+            response = requests.get(url)
+            ebook = scrape_detail_page(response)
+            collection.insert_one(ebook)
         print(ebook)
 
 def scrape_list_page(response):
@@ -29,11 +37,17 @@ def scrape_detail_page(response):
     root = lxml.html.fromstring(response.content)
     ebook = {
         'url':response.url,
+        'key':extract_key(response.url),
         'title':root.cssselect('#bookTitle')[0].text_content(),
-        'price':root.cssselect('.buy')[0].text_content(),
-        'content':[h3.text_content() for h3 in root.cssselect('#content > h3')],
+        'price':root.cssselect('.buy')[0].text.strip(),
+        'content':[normalize_spaces(h3.text_content()) for h3 in root.cssselect('#content > h3')],
     }
     return ebook
+
+def extract_key(url):
+    # URLからキー（URLの末尾のISBN)を抜き出す。
+    m = re.search(r'/([^/]+)$', url)
+    return m.group(1)
 
 def normalize_spaces(s):
     # 連続する空白を１つのスペースに置き換え、前後の空白は削除した新しい文字列を取得する。
